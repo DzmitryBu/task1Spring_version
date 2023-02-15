fc3a1e6bafea9f890384f33773fb78fb1a00b924
Dzmitry Bulavitski <dm_bax@mail.ru>
2023-02-15 21:16:47 +0300

    Spring. Добавил проверку на версию!

--- postgres/ddl/02_genres.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS app.genres
-(
-    id bigserial,
-    name text NOT NULL,
-    version biginit NOT NULL,
-    CONSTRAINT genres_pkey PRIMARY KEY (id)
-)
--- postgres/ddl/03_artists.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS app.artists
-(
-    id bigserial,
-    name text NOT NULL,
-    version biginit NOT NULL,
-    CONSTRAINT artists_pkey PRIMARY KEY (id)
-)
